0eabc70153f0fda3a137c7bb4641b579f2d26522
sanka <a.sotov@yahoo.co.uk>
2023-11-15 19:48:30 +0100

    plot3D.R revised

--- plot3D.R
+++ plot3D.R
@@ -23,6 +23,7 @@ data <- read.table(input_file, sep = "\t", header = TRUE)
 x_values <- data$k
 y_values <- data$p
 z_values <- data$DF
+fr <- data$sum
 
 # Filter data based on conditions
 filtered_data <- subset(data, k < threshold & p < threshold)
@@ -31,6 +32,7 @@ filtered_data <- subset(data, k < threshold & p < threshold)
 x_values <- log(filtered_data$k)  # Apply log scale to x-axis
 y_values <- filtered_data$p
 z_values <- filtered_data$DF
+fr <- filtered_data$sum
 
 # Set up PNG output file
 png(
@@ -49,10 +51,47 @@ par(mar = c(10, 10, 10, 10) + 0.1)  # c(bottom, left, top, right) + extra space
 library(scatterplot3d)
 
 # Create the scatter plot
-scatterplot <- scatterplot3d(x = x_values, y = y_values, z = z_values, main = paste("Input:", input_file, " Output:", output_file, " Threshold =", threshold, "Log scale for k-value."), xlab = colnames(data)[2], ylab = colnames(data)[3], zlab = "DF", pch = 20, angle = 70)
+scatterplot <- scatterplot3d(
+      x = x_values, 
+      y = y_values, 
+      z = z_values, 
+      main = paste("Input:", input_file, " Output:", output_file, " Threshold =", threshold, "Log scale for k-value."), 
+      #clab = "Document\nFrequency",
+      #xlab = "\nNBD k (log scale),
+      #ylab = "\nNBD p",
+      #zlab = "\nDocument Frequency",
+      xlab = colnames(data)[2], 
+      ylab = colnames(data)[3], 
+      zlab = colnames(data)[6],
+      pch = 20, 
+      angle = 20,
+      #cex = 2,
+      #cex.main = 2,
+      #cex.lab = 2,
+      grid=TRUE, 
+      box=FALSE)
+
+# Scale the size variable (fr) to be between a desired range, e.g., 1 and 3
+scaled_size <- 1 + 10 * (fr - min(fr)) / (max(fr) - min(fr))
+
+# Set the transparency level (alpha)
+alpha <- 0.2  # Adjust the transparency level as needed (0 = fully transparent, 1 = opaque)
+
+# Create a color vector with opacity
+point_colors <- rgb(0, 0, 1, alpha = alpha)
+
+# Add points with adjusted size and opacity based on fr
+points(scatterplot$xyz.convert(x_values, y_values, z_values), pch = 20, col = point_colors, cex = scaled_size)
 
 # Add labels from column A to each point
-text(scatterplot$xyz.convert(x_values, y_values, z_values), labels = filtered_data$word, pos = 3, col = "red", cex = 1)
+#text(scatterplot$xyz.convert(x_values, y_values, z_values), labels = filtered_data$word, pos = 3, col = "red", cex = 1)
+
+# Add labels from column A to each point with size dependent on z_values
+normalized_z <- (z_values - min(z_values)) / (max(z_values) - min(z_values))  # Normalize z_values between 0 and 1
+label_size <- 1 + 3 * normalized_z  # Adjust the scaling factor as needed
+
+text(scatterplot$xyz.convert(x_values, y_values, z_values), labels = filtered_data$word, pos = 3, col = "red", cex = label_size)
+
 
 # Save the plot
 dev.off()
